f1876b679404e9bccee46dc3ee6d9e5932b2e2bd
肖扬 <xy_dtb@163.com>
2018-06-05 14:45:32 +0800

    HXB

---
+++ XYHoomXB/XYHoomXB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XYHoomXB
+//
+//  Created by HXB-xiaoYang on 2018/6/5.
+//  Copyright © 2018年 VansXY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
