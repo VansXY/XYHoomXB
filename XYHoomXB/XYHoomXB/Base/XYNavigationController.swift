268606c6acf8bb49db468134856165a608fe4aa2
肖扬 <xy_dtb@163.com>
2018-06-06 19:02:18 +0800

    HXB

--- XYHoomXB/XYHoomXB/Base/XYNavigationController.swift
+++ XYHoomXB/XYHoomXB/Base/XYNavigationController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class XYNavigationController: UINavigationController {
-
+class XYNavigationController: UINavigationController, UIGestureRecognizerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUI()
+        setFullScreenBackGesture()
+        
     }
 
-
+    private func setUI() {
+        navigationBar.shadowImage = UIImage()
+    }
+    
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        if children.count > 0 {
+            viewController.hidesBottomBarWhenPushed = true
+        }
+        super.pushViewController(viewController, animated: animated)
+    }
+    
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return children.count != 1 && value(forKey: "_isTransitioning") as! Bool == false
+    }
+    
+    func setFullScreenBackGesture() {
+        let target = interactivePopGestureRecognizer?.delegate
+        let sel = Selector(("handleNavigationTransition:"))
+        let gesture = UIPanGestureRecognizer(target: target, action: sel)
+        gesture.delegate = self
+        view.addGestureRecognizer(gesture)
+        interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
+    
 }
